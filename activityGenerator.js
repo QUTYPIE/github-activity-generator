b5c5f77095c86d9f798a45e620fac87616e87c6d
₦ł₵₭  ₣ɄⱤɎ <partham302@gmail.com>
2024-11-20 13:46:43 +0000

    git push -u origin main

--- activityGenerator.js
+++ activityGenerator.js
@@ -5,7 +5,7 @@ const path = require("path");
 // Configuration
 const DAYS = 1;              // Number of days to go back
 const COMMITS_PER_DAY = 1000; // Number of commits per day
-const FILE_NAME = "activity.sas"; // File to change for each commit
+const FILE_NAME = "activity.pro"; // File to change for each commit
 const SRC_DIR = path.join(__dirname, "src");
 
 // Ensure the `src` directory exists
