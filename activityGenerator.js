ed5abd4db6fd6a141c34b902e7907624d60c9874
₦ł₵₭  ₣ɄⱤɎ <partham302@gmail.com>
2024-11-24 12:25:14 +0000

    git push -u origin main

--- activityGenerator.js
+++ activityGenerator.js
@@ -5,7 +5,7 @@ const path = require("path");
 // Configuration
 const DAYS = 1;              // Number of days to go back
 const COMMITS_PER_DAY = 900; // Number of commits per day
-const FILE_NAME = "SATSDB.go"; // File to change for each commit
+const FILE_NAME = "SATSDB.php"; // File to change for each commit
 const SRC_DIR = path.join(__dirname, "src/SATADB");
 
 // Ensure the `src` directory exists
