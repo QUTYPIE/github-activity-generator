890d4544ddf02395f25266278f454c4ca7dc09c4
₦ł₵₭  ₣ɄⱤɎ <96591657+NICK-FURY-6023@users.noreply.github.com>
2024-12-22 12:59:26 +0000

    Commit #:

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ try {
 
 // Configuration
 const DAYS = 1; // Number of days to go back
-const COMMITS_PER_DAY = 100; // Number of commits per day
+const COMMITS_PER_DAY = 1000; // Number of commits per day
 const SRC_DIR = path.join(__dirname, "src/main/database/formart");
 
 // Ensure the `src` directory exists
