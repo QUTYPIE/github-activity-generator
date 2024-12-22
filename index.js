d518c492c6ea2593f22dafc4457e7009bf36a7f1
₦ł₵₭  ₣ɄⱤɎ <96591657+NICK-FURY-6023@users.noreply.github.com>
2024-12-22 14:14:43 +0000

    // Extra file path "/main/database/formart;"

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ try {
 // Configuration
 const DAYS = 1; // Number of days to go back
 const COMMITS_PER_DAY = 1000; // Number of commits per day
-const SRC_DIR = path.join(__dirname, "src/main/database/formart");
+const SRC_DIR = path.join(__dirname, "src"); // Extra file path "/main/database/formart;"
 
 // Ensure the `src` directory exists
 if (!fs.existsSync(SRC_DIR)) {
