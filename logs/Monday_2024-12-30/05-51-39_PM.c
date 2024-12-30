aeea907c69b983f83a7244febfb643c78333ab6f
₦ł₵₭  ₣ɄⱤɎ <96591657+NICK-FURY-6023@users.noreply.github.com>
2024-12-30 17:08:44 +0000

    Remove unused logging file from the project

--- logs/Monday_2024-12-30/05-51-39_PM.c
+++
@@ -1,5 +0,0 @@
-const = "@discordjs/opus";
-import { createWriteStream } from "fs";
-import { join } from "path";
-import { createLogger, format, transports } from "winston";
-import { Console } from "winston/lib/winston/transports";
